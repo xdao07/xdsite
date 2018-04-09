6d160aefb981ea8c73d800271e1e49b3e9aea61a
xdao07 <xdao07@163.com>
2018-04-09 20:39:15 +0800

    基础模块开发和首页动态数据效果

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView   # 通用视图，可直接将静态映射为视图函数
+from  . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='blog/index.html'), name='home'),
+    url(r'^$', views.index, name='home'),
     url(r'^aboutme/$', TemplateView.as_view(template_name='blog/aboutme.html'), name='aboutme'),
     url(r'^messageme/$', TemplateView.as_view(template_name='blog/messageme.html'), name='messageme'),
     url(r'^articlelist/$', TemplateView.as_view(template_name='blog/articlelist.html'), name='articlelist'),
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.db.models import Q  # filter(~Q(name=''))过滤表示不等于
+from .models import Category, Tag, Article, Comment, Links
 
-# Create your views here.
+def global_setting(request):
+    """
+    定义全局变量函数，将函数中的变量通过locals()返回
+    将global_setting函数加到 settings.py/TEMPLATES/OPTIONS 的上下文处理器中
+    """
+    # 获取所有可以显示的标签对象
+    tags = Tag.objects.filter(is_display=1).all()
+    # 获取所有友情链接对象
+    links = Links.objects.all()
+
+    # 图文推荐文章列表对象
+    recommend_articles = Article.objects.filter(~Q(picture='')).filter(is_recommend=1).order_by('-hits')[:5]
+
+    # 点击排行版文章列表对象
+    hits_articles = Article.objects.order_by('-hits')[:10]
+
+    # 定义导航分类
+    # 从数据库获取指定ID的分类对象列表（置于技术杂谈导航下）
+    nav_jszt_category = Category.objects.filter(id__in=settings.NAV_JSZT_CATEGORY).order_by('-index')
+
+    # 从数据库获取指定ID的分类对象列表（置于生活随笔导航下）
+    nav_shsb_category = Category.objects.filter(id__in=settings.NAV_SHSB_CATEGORY).order_by('-index')
+
+    # settings中站点基本信息配置
+    site_name = settings.SITE_NAME
+    site_keywords = settings.SITE_KEYWORDS
+    site_desc = settings.SITE_DESC
+    site_beian = settings.SITE_BEIAN
+
+    pro_mail = settings.PRO_EMAIL
+
+    return locals()
+
+# 首页视图
+# 对当前视图的输出进行缓存，时间15分钟（推荐在conf中指定视图缓存）
+# @cache_page(60*15)
+def index(request):
+    articles = Article.objects.all()
+
+    return render(request, 'blog/index.html', locals())
+
+
