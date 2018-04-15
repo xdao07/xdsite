5f622b4cfd29e34beae1785b5cbbc3070748931c
xdao07 <xdao07@163.com>
2018-04-15 17:41:55 +0800

    基础模块开发和首页动态数据效果

--- xdsite/settings.py
+++ xdsite/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blog.views.global_setting',   # 将global_setting中定义的全局变量加入上下文处理器中
             ],
         },
     },
@@ -133,3 +134,21 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # 允许上传文件类型['jpg', 'png', 'jpeg']
 ALLOW_SUFFIX = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'txt', 'html']
+
+"""
+自定义变量
+"""
+# 导航【技术杂谈】分类id列表
+NAV_JSZT_CATEGORY = (1, 2, 3, 4, 5)
+# 导航【生活随笔】分类id列表
+NAV_SHSB_CATEGORY = (6, 7)
+# 分页，每页显示记录条数
+PER_PAGE = 2
+
+# 网站基本信息
+SITE_NAME = u'Django个人博客——江南墨卷'
+SITE_HTTP = u'http://www.example.net'
+SITE_DESC = u'天将降大任于是人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能。'
+SITE_KEYWORDS = u'运维，开发，Linux，数据库'
+SITE_BEIAN = u'粤ICP备11111111号'
+PRO_EMAIL = u'admin@example.cn'
